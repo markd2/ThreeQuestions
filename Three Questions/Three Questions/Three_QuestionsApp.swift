ec1a01e56e74ee8c6927002c7f1a23063a4094ea
Mark Dalrymple <markd@borkware.com>
2023-12-18 17:18:15 -0500

    adds new xcode project from template

---
+++ Three Questions/Three Questions/Three_QuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_QuestionsApp.swift
+//  Three Questions
+//
+//  Created by markd on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_QuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
